baa5aa53e9b0fd0a27b22bc99a4d66e0ba8d7582
Kristian Helf <kristian.helf@gmx.net>
2026-05-17 00:49:09 +0200

    Modified reader lambda to query data based on dates rather than everything

--- lambda/reader.py
+++ lambda/reader.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+import re
 from boto3.dynamodb.conditions import Key
 
 dynamodb = boto3.resource('dynamodb', region_name='eu-north-1')
@@ -7,14 +8,31 @@ table = dynamodb.Table('motor-metrics')
 
 def lambda_handler(event, context):
     motor_id = event['queryStringParameters']['motor_id']
-    response = table.query(KeyConditionExpression=Key('motor_id').eq(motor_id))
-    items = [item for item in response['Items'] if item['timestamp'] != 'CONFIG']
+    date = event['queryStringParameters']['date']
+    regex = "^\d{4}-\d{1,2}-\d{1,2}$"
 
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        },
-        'body': json.dumps(items, default=str)
-    }
+    if(not re.match(regex, date)):
+        return {
+            'statusCode': 400,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': json.dumps('Bad Request')
+        }
+    else:
+        start = f"{date}T00:00:00Z"
+        end = f"{date}T23:59:59Z"
+        response = table.query(
+            KeyConditionExpression=Key('motor_id').eq(motor_id) & Key('timestamp').between(start, end)
+        )
+        items = [item for item in response['Items'] if item['timestamp'] != 'CONFIG']
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': json.dumps(items, default=str)
+        }
